559a3480a0627f8d8530e73c0c3d1e27c9ccbeae
Erik Brodin <erik.brodin@colostate.edu>
2022-04-18 09:38:01 -0600

    Add Chombo style to C-style

--- init.el
+++ init.el
@@ -42,6 +42,46 @@
 (global-set-key (kbd "C-x a") 'align)
 (show-paren-mode)
 
+;; Enable c++-mode for .cu files
+(setq auto-mode-alist
+  (append
+    '(
+      ;; File name ends in `.h'.
+      ("\\.h\\'" . c++-mode)
+      ;; File name ends in `.cu'.
+      ("\\.cu\\'" . c++-mode)
+      ;; File name ends in `.ino'.
+      ("\\.ino\\'" . c++-mode)
+      ;; File name ends in `.yy'.
+      ("\\.yy\\'" . c++-mode)
+      ;; File name end in `.CHF'
+      ("\\.ChF\\'" . fortran-mode)
+      ;; File name end in `.m'
+      ("\\.m\\'" . octave-mode)
+    )
+    auto-mode-alist))
+
+(defconst Chombo-style
+  '((c-tab-always-indent               . t)
+    (c-basic-offset                    . 2)
+    (c-comment-only-line               . 0)
+    (continued-statement-offset        . 2)
+    (c-continued-brace-offset          . 0)
+    (c-brace-offset                    . 2)
+    (c-brace-imaginary-offset          . 0)
+    (c-argdecl-indent                  . 2)
+    (c-label-offset                    . -2)
+    (c++-member-init-indent            . 2)
+    (c++-continued-member-init-offset  . 0)
+    (c++-empty-arglist-indent          . 2)
+    (c++-friend-offset                 . 0)
+    )
+  "Chombo Style")
+
+
+
+
+
 ;; Now we load all enabled modules in the order dictated by your `doom!' block
 ;; in $DOOMDIR/init.el. `doom-initialize-modules' loads them (and hooks) in the
 ;; given order:
@@ -61,3 +101,11 @@
 ;; And then we're good to go!
 (doom-initialize-modules)
 
+;; Customizations for all of c-mode, c++-mode, and objc-mode
+(defun my-c-mode-common-hook ()
+  ;; add my personal style and set it for the current buffer
+  (c-add-style "Chombo" Chombo-style t))
+
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+
+(setq c-default-style "Chombo")
